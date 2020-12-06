cc8046220129aaff78eae3fb23c676e353957d50
aditya-nurfauzi1 <aditya.muh1907@gmail.com>
2020-12-06 17:06:48 +0700

    Menambahkan kode di kelas Tim dan menambah kelas TimMain

---
+++ src/main/java/org/ifunpas/kpl/kode/Pemain.java
@@ -0,0 +1,26 @@
+package org.ifunpas.kpl.kode;
+
+public class Pemain {
+	private String nama;
+	private int noPunggung;
+	
+	public Pemain(String nama, int noPunggung) {
+		this.nama = nama;
+		this.noPunggung = noPunggung;
+	}
+	
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	
+	public String getNama() {
+		return nama;
+	}
+	public void setNoPunggung(int noPunggung) {
+		this.noPunggung = noPunggung;
+	}
+	
+	public int getNoPunggung() {
+		return noPunggung;
+	}
+}
---
+++ src/main/java/org/ifunpas/kpl/kode/Tim.java
@@ -0,0 +1,47 @@
+package org.ifunpas.kpl.kode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tim {
+	private String nama;
+	private List<Pemain> pemainInti = new ArrayList<Pemain>();
+	
+	public Tim() {
+		nama = "";
+	}
+	
+	public Tim(String nama) {
+		this.nama = nama;
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public List<Pemain> getPemainInti() {
+		return pemainInti;
+	}
+
+	public void setPemainInti(List<Pemain> pemainInti) {
+		this.pemainInti = pemainInti;
+	}
+	
+	public boolean tambahPemainInti(Pemain pemain) {
+		if(pemainInti.size() < 11) {
+			pemainInti.add(pemain);
+			System.out.println(pemain.getNama() + "berhasil ditambahkan sebagai pemain inti.");
+			return true;
+		}
+		else {
+			System.out.println(pemain.getNama() + "tidak berhasil ditambahkan sebagai pemmain inti.");
+			return false;
+		}
+	}
+	
+	
+}
